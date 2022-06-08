cb722b81fe1a62033433b65e298d0b49faa56d4d
Francis <f@francis.fish>
2022-06-08 19:10:37 +0100

    Configure rspec

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,15 @@ module Postcode
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.helper_specs false
+      g.controller_specs false
+      g.view_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
+
   end
 end
