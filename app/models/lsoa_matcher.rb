a21e8df6ba492a33cd5bec612a242e093490c8ba
Francis <f@francis.fish>
2022-06-12 16:05:21 +0100

    Add matching capabilities to matcher model

--- app/models/lsoa_matcher.rb
+++ app/models/lsoa_matcher.rb
@@ -3,4 +3,38 @@ class LsoaMatcher < ApplicationRecord
   before_validation do
     self.extra_postcodes ||= []
   end
+
+  def has_postcode(postcode)
+    check_extra(postcode) || has_match(postcode)
+  end
+
+  private
+
+  def check_extra(postcode)
+    normal(postcode).in?(normalised_extra)
+  end
+
+  def normal(postcode)
+    postcode.gsub(/[[:space:]]/, "").downcase
+  end
+
+  def normalised_extra
+    @normalised_extra ||= extra_postcodes.map(&method(:normal))
+  end
+
+  def has_match(postcode)
+    postcode_details = PostcodeFetcher.fetch(postcode)
+    match_regexp.each do |re|
+      return true if postcode_details.lsoa&.match?(re)
+    end
+    false
+  end
+
+  def match_regexp
+    @match_regexp ||=
+      begin
+        to_re = ->(string) { Regexp.new(string, Regexp::IGNORECASE) }
+        match_strings.map(&to_re)
+      end
+  end
 end
--- spec/models/lsoa_matcher_spec.rb
+++ spec/models/lsoa_matcher_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe LsoaMatcher, type: :model do
   context "model validation" do
-    let (:subject) { FactoryBot.create(:lsoa_matcher, match_strings: ["test"] ) }
+    let(:subject) { FactoryBot.create(:lsoa_matcher, match_strings: ["test"]) }
     it "intialises" do
       subject
     end
@@ -30,6 +30,66 @@ RSpec.describe LsoaMatcher, type: :model do
       subject.extra_postcodes = nil
       expect(subject).to be_valid
       expect(subject.extra_postcodes).to eq []
+    end
+  end
+  context "matching" do
+    let(:subject) { FactoryBot.create(:lsoa_matcher, match_strings: ["test"]) }
+    context "One match string set" do
+      before :each do
+        postcode = double("postcode", lsoa: "test")
+        allow(PostcodeFetcher).to receive(:fetch).and_return(postcode)
+      end
+      it "simple fail" do
+        postcode = double("postcode", lsoa: "fail")
+        allow(PostcodeFetcher).to receive(:fetch).and_return(postcode)
+        expect(subject.has_postcode("test")).to be false
+      end
+      it "simple match" do
+        expect(subject.has_postcode("test")).to be true
+      end
+      it "ignores case" do
+        expect(subject.has_postcode("TeSt")).to be true
+      end
+      it "matches anywhere in the string" do
+        expect(subject.has_postcode("some TeSt")).to be true
+      end
+    end
+    context "Two match string set" do
+      before :each do
+        subject.match_strings << "test2"
+        postcode = double("postcode", lsoa: "test2")
+        allow(PostcodeFetcher).to receive(:fetch).and_return(postcode)
+      end
+      it "simple fail" do
+        postcode = double("postcode", lsoa: "fail")
+        allow(PostcodeFetcher).to receive(:fetch).and_return(postcode)
+        expect(subject.has_postcode("test2")).to be false
+      end
+      it "simple match" do
+        expect(subject.has_postcode("test2")).to be true
+      end
+      it "ignores case" do
+        expect(subject.has_postcode("TeSt2")).to be true
+      end
+      it "matches anywhere in the string" do
+        expect(subject.has_postcode("some TeSt2")).to be true
+      end
+    end
+  end
+  context "extra postcodes" do
+    let(:subject) { FactoryBot.create(:lsoa_matcher, match_strings: ["nope"], extra_postcodes: %w[test1 test2]) }
+    context "One match string set" do
+      before :each do
+        expect(PostcodeFetcher).not_to receive(:fetch)
+      end
+      it "matches saved no spaces" do
+        expect(subject.has_postcode("test1")).to be true
+        expect(subject.has_postcode("test2")).to be true
+      end
+      it "matches saved with spaces" do
+        expect(subject.has_postcode("test 1")).to be true
+        expect(subject.has_postcode("test 2")).to be true
+      end
     end
   end
 end
